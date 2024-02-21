da77c2df6214ee884afdba1ec038faaa472e5337
kradhenz <tomas25498@gmail.com>
2024-02-21 01:05:11 -0300

    :sparkles: added code for multiple clients

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
 import Client from './client.js';
 import Taxes from './taxes.js';
 
-const taxClient1 = new Taxes(100000, 20000);
-const client1 = new Client("Jane");
-client1.tax = taxClient1;
+// define tax data for each client in array
+const customerData = [ // aga: Annual Gross Amount
+    { name: "Jane", aga: 100000, deductions: 20000 },
+    { name: "William", aga: 150000, deductions: 30000 },
+    { name: "Alba", aga: 80000, deductions: 10000 }
+];
 
-console.log(`El impuesto a pagar por ${client1.name} es: $${client1.calculateTax().toFixed(2)}`);
+// Create customer instances and assign tax data
+const customers = customerData.map(data => {
+    const taxes = new Taxes(data.aga, data.deductions);
+    return new Client(data.name, taxes);
+});
 
+// show the tax calculated for each client
+customers.forEach(customer => {
+    console.log(`El impuesto a pagar por ${customer.name} es: $${customer.calculateTax().toFixed(2)}`);
+});
