425b7d9b5ce169603522df586ebf11a10f323435
kradhenz <tomas25498@gmail.com>
2024-02-20 21:37:04 -0300

    Revert ":tada: Basic Structure, install babel: config package.json"
    This reverts commit 1d72e7001935b64e56892a72bb5911361d028ef6.

--- dist/js/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _client = require("./client.js");
-require("./taxes.js");
-console.log('resta', (0, _client.resta)(50, 30));
-console.log('multiplicacion', (0, _client.multiplicacion)(10, 5));
